5e62bf7161cfb80bbdb94b245c573269f2840f6f
lpkll <lpkll123@gmail.com>
2026-05-29 20:54:53 +0700

    Polish code

---
+++ src/client/java/net/riiael/mixin/client/packet/ClientPacketListenerMixin.java
@@ -0,0 +1,37 @@
+package net.riiael.mixin.client.packet;
+
+import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+import java.util.Objects;
+
+@Mixin(ClientPacketListener.class)
+public abstract class ClientPacketListenerMixin extends ClientCommonPacketListenerImpl
+{
+    @Shadow
+    private ClientLevel level;
+
+    protected ClientPacketListenerMixin()
+    {
+        super(null, null, null);
+    }
+
+    @Redirect(method = "handleSetEntityData", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundSetEntityDataPacket;packedItems()Ljava/util/List;"))
+    public List<SynchedEntityData.DataValue<?>> res(ClientboundSetEntityDataPacket packet)
+    {
+        if (level.getEntity(packet.id()) == minecraft.player)
+        {
+            packet.packedItems().removeIf(v -> v.serializer() == EntityDataSerializers.POSE);
+        }
+        return packet.packedItems();
+    }
+}
